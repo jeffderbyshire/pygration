32ea390509bbef8fb80a26e23bf43c7c5500af85
jeffderb <jeffderb@fnal.gov>
2019-10-07 12:14:27 -0500

    debug path

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -1,4 +1,8 @@
+import sys
 from migrate_helper_scripts import migrationLogs
+
+sys.path.append('/home/users/jeffderb/bin/tape-migration/migrate_helper_scripts')
+sys.path.append('/home/users/jeffderb/bin/tape-migration')
 
 migrationLogs.too_many_logs('test', list('VPV008'))
 
