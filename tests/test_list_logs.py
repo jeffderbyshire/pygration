839ba328db935cc487edf9347943a510702f21c8
jeffderb <jeffderb@fnal.gov>
2019-10-07 14:25:45 -0500

    change test

--- tests/test_list_logs.py
+++ tests/test_list_logs.py
@@ -1,5 +1,5 @@
 import pprint
 from migrate_helper_scripts import list_logs
 
-logs = list_logs.get_logs("errors", volumes=list('VPV008'))
+logs = list_logs.get_logs("errors", list('VPV008'))
 pprint.pprint(logs)
