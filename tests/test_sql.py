7b2cc9cfa4ff27b97028444c0eefeee772619a51
jeffderb <jeffderb@fnal.gov>
2019-10-07 14:32:14 -0500

    fix list input

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -4,5 +4,5 @@ from migrate_helper_scripts import migrationLogs
 sys.path.append('/home/users/jeffderb/bin/tape-migration/migrate_helper_scripts')
 sys.path.append('/home/users/jeffderb/bin/tape-migration')
 
-migrationLogs.too_many_logs('test', list('VPV008'))
+migrationLogs.too_many_logs('test', ['VPV008'])
 
