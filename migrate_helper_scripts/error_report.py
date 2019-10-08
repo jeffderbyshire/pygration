c126721bbb6a5e49095b9e022baa9b8956ef243b
jeffderb <jeffderb@fnal.gov>
2019-10-08 16:48:22 -0500

    try to create 2d matrix

--- migrate_helper_scripts/error_report.py
+++ migrate_helper_scripts/error_report.py
@@ -16,16 +16,24 @@ def push():
                    "ORDER BY a.server, b.volume ")
     results = cursor.fetchall()
     wks.clear('A2')
+    row_values = []
+    all_values = []
     # report_file = open('/home/users/jeffderb/db/error_report.csv', 'w')
-    #for row in results:
+    for row in results:
+        row_values['server'] = row[0]
+        row_values['volume'] = row[1]
+        row_values['log_file'] = row[2]
+        row_values['message'] = row[3]
+        row_values['date'] = row[4]
+        row_values['snippet'] = row[5]
+        all_values.append(row_values)
     #    report_file.write("|".join(row) + "\n")
-    #report_file.close()
-    pprint.pprint(results)
+    # pprint.pprint(results)
     try:
-        wks.update_values('A2', results, extend=True)
+        wks.update_values('A2', all_values, extend=True)
     except:
-        cells = 'A2:N' + str(len(results) + 10)
-        wks.update_values(cells, results, extend=True)
+        cells = 'A2:N' + str(len(all_values) + 10)
+        wks.update_values(cells, all_values, extend=True)
     # wks.update_values('A2', results)
     return 'report to /home/users/jeffderb/db/error_report'
 
