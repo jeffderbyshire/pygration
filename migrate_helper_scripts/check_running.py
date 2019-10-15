718ccef64534685739e31ac41ff04866507ffacf
jeffderb <jeffderb@fnal.gov>
2019-10-15 14:11:28 -0500

    check for running migrations

---
+++ migrate_helper_scripts/check_running.py
@@ -0,0 +1,14 @@
+import subprocess
+
+
+def main():
+    volumes_running = []
+    with subprocess.Popen(["/var/migration/migration-bin/dmPs.sh"], stdout=PIPE) as proc:
+        volumes_running.append(proc.stdout.read()[-1])
+
+    return volumes_running
+
+
+if __name__ == "__main__":
+    import pprint
+    pprint.pprint(main())
