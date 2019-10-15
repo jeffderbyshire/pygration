7f6b61b66352f44990693afc85ac85df26f0bfa7
jeffderb <jeffderb@fnal.gov>
2019-10-15 14:40:26 -0500

    debug

--- migrate_helper_scripts/check_running.py
+++ migrate_helper_scripts/check_running.py
@@ -3,8 +3,8 @@ import subprocess
 
 def main():
     volumes_running = []
-    with subprocess.run(['ps', 'waux'], capture_output=True) as proc:
-        volumes_running.append(proc.stdout)
+    process = subprocess.run(['ps', 'waux'], capture_output=True)
+    volumes_running = process.stdout
 
     return volumes_running
 
