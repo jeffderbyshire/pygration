e42c82b2c383aee22ff15c49345ee13145635865
jeffderb <jeffderb@fnal.gov>
2019-10-15 15:40:38 -0500

    debug

--- migrate_helper_scripts/check_running.py
+++ migrate_helper_scripts/check_running.py
@@ -1,15 +1,11 @@
-from subprocess import Popen, PIPE
+import sh
 
 
 def main():
     volumes_running = []
-    process_ps = Popen(['ps', '-ww'],
-                       stdout=PIPE)
-    process_grep = Popen(['grep', 'migrate_chimera'], stdin=process_ps.stdout, stdout=PIPE)
+    process = sh.grep(sh.ps('w'), 'migrate_chimera')
 
-    process_ps.stdout.close()  # Allow proc1 to receive a SIGPIPE if proc2 exits.
-    out, err = process_grep.communicate()
-    return out
+    return process
 
 
 if __name__ == "__main__":
