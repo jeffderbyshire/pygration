e9006a853a413aadffa7fc4bdedac882950943c5
jeffderb <jeffderb@fnal.gov>
2019-10-15 17:17:48 -0500

    handle exception in sh module

--- migrate_helper_scripts/check_running.py
+++ migrate_helper_scripts/check_running.py
@@ -3,7 +3,10 @@ import sh
 
 def main():
     volumes_running = []
-    process = sh.grep(sh.ps('ww'), 'migrate_chimera')
+    try:
+        process = sh.grep(sh.ps('ww'), 'migrate_chimera')
+    except sh.ErrorReturnCode:
+        return volumes_running
     for line in process:
         volumes_running.append(line.split()[-1])
 
