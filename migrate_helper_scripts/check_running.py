a44a0d89a69f5df8fa3c8b5c6ea45f77e9e802c0
jeffderb <jeffderb@fnal.gov>
2019-10-15 14:50:27 -0500

    debug

--- migrate_helper_scripts/check_running.py
+++ migrate_helper_scripts/check_running.py
@@ -4,7 +4,7 @@ import subprocess
 def main():
     volumes_running = []
     process = subprocess.run(['/var/migration/migration-bin/dmPs.sh'], check=True,
-                             stdout=subprocess.PIPE, universal_newlines=True)
+                             shell=True, stdout=subprocess.PIPE, universal_newlines=True)
 
     return process.stdout
 
