f6ca3488402c248e2b5bf919bdfadf596c093aa1
jeffderb <jeffderb@fnal.gov>
2019-10-15 16:02:49 -0500

    don't write the file or run if more than 2 processes running

--- migrate_helper_scripts/build_rerun.py
+++ migrate_helper_scripts/build_rerun.py
@@ -8,38 +8,36 @@ IGNORE = ['--scan', '--restore']
 
 
 def rerun(volumes):
-    run_ok = True
     volumes_added = check_running.main()
-    if len(volumes_added) > 1:
-        run_ok = False
-    volumes_rerun = []
-    os.chmod(RERUN_SCRIPT, 0o700)
-    file = open(RERUN_SCRIPT, "w")
-    header = "#!/usr/bin/env bash\ncd /var/migration\nsource ~enstore/.bashrc\n"
-    file.write(header)
-    logs = list_logs.get_logs('errors', volumes)
-    for log in logs:
-        command = ''
-        file_name = MIGRATION_DIR + log
-        with open(file_name, 'rb') as fh:
-            first = next(fh).decode().split()
-            volume = first[-1]
-            if volume not in volumes_added:
-                command = "/opt/enstore/Python/bin/python " + " ".join(first[7:])
-                for keyword in IGNORE:
-                    if keyword in command:
-                        command = ''
-                        break
-                volumes_added.append(volume)
-        if command != '':
-            file.write(command + '\n')
-            volumes_rerun.append(command.split()[-1])
-    if run_ok:
+    if len(volumes_added) < 2:
+        volumes_rerun = []
+        os.chmod(RERUN_SCRIPT, 0o700)
+        file = open(RERUN_SCRIPT, "w")
+        header = "#!/usr/bin/env bash\ncd /var/migration\nsource ~enstore/.bashrc\n"
+        file.write(header)
+        logs = list_logs.get_logs('errors', volumes)
+        for log in logs:
+            command = ''
+            file_name = MIGRATION_DIR + log
+            with open(file_name, 'rb') as fh:
+                first = next(fh).decode().split()
+                volume = first[-1]
+                if volume not in volumes_added:
+                    command = "/opt/enstore/Python/bin/python " + " ".join(first[7:])
+                    for keyword in IGNORE:
+                        if keyword in command:
+                            command = ''
+                            break
+                    volumes_added.append(volume)
+            if command != '':
+                file.write(command + '\n')
+                volumes_rerun.append(command.split()[-1])
         os.system('screen -d -m ' + RERUN_SCRIPT)
+        file.close()
+        return len(volumes_rerun)
     else:
         print('too many migrate_chimera processes running')
-    file.close()
-    return len(volumes_rerun)
+        return 0
 
 
 if __name__ == '__main__':
