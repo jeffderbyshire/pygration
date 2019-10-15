43d496592a054ce4c0ffb2388d2dce6bf1c60dae
jeffderb <jeffderb@fnal.gov>
2019-10-15 15:29:37 -0500

    debug

--- migrate_helper_scripts/check_running.py
+++ migrate_helper_scripts/check_running.py
@@ -7,7 +7,9 @@ def main():
                        stdout=PIPE)
     process_grep = Popen(['grep', 'migrate_chimera'], stdin=process_ps.stdout, stdout=PIPE)
 
-    return process_grep.stdout
+    process_ps.stdout.close()  # Allow proc1 to receive a SIGPIPE if proc2 exits.
+    out, err = process_grep.communicate()
+    return out
 
 
 if __name__ == "__main__":
