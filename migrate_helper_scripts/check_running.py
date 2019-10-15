4bc22c75d7c8e9481970109c95ec419b495213eb
jeffderb <jeffderb@fnal.gov>
2019-10-15 15:28:24 -0500

    debug

--- migrate_helper_scripts/check_running.py
+++ migrate_helper_scripts/check_running.py
@@ -3,8 +3,8 @@ from subprocess import Popen, PIPE
 
 def main():
     volumes_running = []
-    process_ps = Popen(['ps', 'w'], check=True,
-                    stdout=PIPE, text=True)
+    process_ps = Popen(['ps', 'w'],
+                       stdout=PIPE)
     process_grep = Popen(['grep', 'migrate_chimera'], stdin=process_ps.stdout, stdout=PIPE)
 
     return process_grep.stdout
