c0515e3ef3f4131f5614b882af70dbd0bab18905
jeffderb <jeffderb@fnal.gov>
2019-10-16 13:11:57 -0500

    debug

--- migrate_helper_scripts/check_logs.sh
+++ migrate_helper_scripts/check_logs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 MAILTO=jeffderb@fnal.gov
+eval "$(conda shell.bash hook)"
 /opt/miniconda3/bin/conda activate base
 python /root/migration_scripts/tape-migration/pygration.py --process check --quiet
 exit
