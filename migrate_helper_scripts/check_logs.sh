d36df606e82e3ca9d4b579698d0424a9ed039f7f
jeffderb <jeffderb@fnal.gov>
2019-10-16 13:15:46 -0500

    add source /opt/miniconda3/etc/profile.d/conda.sh

--- migrate_helper_scripts/check_logs.sh
+++ migrate_helper_scripts/check_logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MAILTO=jeffderb@fnal.gov
-eval "$(conda shell.bash hook)"
+source /opt/miniconda3/etc/profile.d/conda.sh
 /opt/miniconda3/bin/conda activate base
 python /root/migration_scripts/tape-migration/pygration.py --process check --quiet
 exit
