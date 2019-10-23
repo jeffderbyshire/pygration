e516f5a802c4352072d47b0c24ce74aa1e3dd2b0
jeffderb <jeffderb@fnal.gov>
2019-10-23 16:05:49 -0500

    check spool directory for each command and if less than 60 % used then write command to file otherwise skip command.

--- migrate_helper_scripts/build_rerun.py
+++ migrate_helper_scripts/build_rerun.py
@@ -5,51 +5,42 @@ from . import check_running
 RERUN_SCRIPT = '/tmp/migrate.rerun'
 MIGRATION_DIR = '/var/migration/'
 IGNORE = ['--scan', '--restore']
-SPOOL_DIR_FDM = '/data/data3'
-SPOOL_DIR_STK = '/data/data1'
 
 
 def rerun(volumes):
     volumes_added = check_running.main()
-    # TODO check for disk space used < 60%?
-    if os.path.exists(SPOOL_DIR_FDM):
-        total, used, free = shutil.disk_usage(SPOOL_DIR_FDM)
-    elif os.path.exists(SPOOL_DIR_STK):
-        total, used, free = shutil.disk_usage(SPOOL_DIR_STK)
+    if len(volumes_added) < 2:
+        volumes_rerun = []
+        os.chmod(RERUN_SCRIPT, 0o700)
+        file = open(RERUN_SCRIPT, "w")
+        header = "#!/usr/bin/env bash\ncd /var/migration\nsource ~enstore/.bashrc\n"
+        file.write(header)
+        logs = list_logs.get_logs('errors', volumes)
+        for log in logs:
+            command = ''
+            file_name = MIGRATION_DIR + log
+            with open(file_name, 'rb') as fh:
+                first = next(fh).decode().split()
+                volume = first[-1]
+                for argument in first:
+                    if '/data/data' in argument:
+                        spool = argument.split('/')
+                        total, used, free = shutil.disk_usage("/".join(spool[0:3]))
+                if volume not in volumes_added and used/total < 0.60:
+                    command = "/opt/enstore/Python/bin/python " + " ".join(first[7:])
+                    for keyword in IGNORE:
+                        if keyword in command:
+                            command = ''
+                            break
+                    volumes_added.append(volume)
+            if command != '':
+                file.write(command + '\n')
+                volumes_rerun.append(command.split()[-1])
+        os.system('screen -d -m ' + RERUN_SCRIPT)
+        file.close()
+        return len(volumes_rerun)
     else:
-        total = used = 1
-    if used/total < 0.60:
-        if len(volumes_added) < 2:
-            volumes_rerun = []
-            os.chmod(RERUN_SCRIPT, 0o700)
-            file = open(RERUN_SCRIPT, "w")
-            header = "#!/usr/bin/env bash\ncd /var/migration\nsource ~enstore/.bashrc\n"
-            file.write(header)
-            logs = list_logs.get_logs('errors', volumes)
-            for log in logs:
-                command = ''
-                file_name = MIGRATION_DIR + log
-                with open(file_name, 'rb') as fh:
-                    first = next(fh).decode().split()
-                    volume = first[-1]
-                    if volume not in volumes_added:
-                        command = "/opt/enstore/Python/bin/python " + " ".join(first[7:])
-                        for keyword in IGNORE:
-                            if keyword in command:
-                                command = ''
-                                break
-                        volumes_added.append(volume)
-                if command != '':
-                    file.write(command + '\n')
-                    volumes_rerun.append(command.split()[-1])
-            os.system('screen -d -m ' + RERUN_SCRIPT)
-            file.close()
-            return len(volumes_rerun)
-        else:
-            print('too many migrate_chimera processes running')
-            return 0
-    else:
-        print('Not enough disk space')
+        print('too many migrate_chimera processes running')
         return 0
 
 
