7095e87a2a21a0dcf41b6f6c01e99fbe91a28195
jeffderb <jeffderb@fnal.gov>
2019-10-31 11:27:29 -0500

    debug

--- migrate_helper_scripts/archive_logs.py
+++ migrate_helper_scripts/archive_logs.py
@@ -25,8 +25,8 @@ def check_migration_status(volumes):
     for volume in volumes:
         status = subprocess.run(['/opt/enstore/Python/bin/python', '/opt/enstore/bin/enstore',
                                 'info', '--vol', volume], capture_output=True)
-        print(status.stdout.decode())
-        check = json.dumps(status.stdout.decode())
+        print(status.stdout.decode().replace("'", '"'))
+        check = json.dumps(status.stdout.decode().replace("'", '"'))
         pprint.pprint(check)
         sys.exit()
 
