79d91dc37cfc32631a10a1c95cf10c5bd5efc3b8
jeffderb <jeffderb@fnal.gov>
2019-10-31 11:11:39 -0500

    debug

--- migrate_helper_scripts/archive_logs.py
+++ migrate_helper_scripts/archive_logs.py
@@ -22,7 +22,7 @@ def check_migration_status(volumes):
     for volume in volumes:
         status = subprocess.run(['/opt/enstore/Python/bin/python', '/opt/enstore/bin/enstore',
                                 'info', '--vol', volume], capture_output=True)
-        print(status.stdout.decode())
+        print(status.stdout.decode()['system_inhibit'][1])
         sys.exit()
 
 
