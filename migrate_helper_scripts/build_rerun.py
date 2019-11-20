0232d12473a31dd44e14a621311f3f9003dbca18
jeffderb <jeffderb@fnal.gov>
2019-11-19 19:00:01 -0600

    refactoring

--- migrate_helper_scripts/build_rerun.py
+++ migrate_helper_scripts/build_rerun.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import migrate_helper_scripts.check_running as check_running
 import migrate_helper_scripts.progress_bar as progress_bar
+import migrate_helper_scripts.list_logs as list_logs
 
 
 RERUN_SCRIPT = '/tmp/migrate.rerun'
@@ -16,54 +17,48 @@ DATA1 = '/data/data1'
 
 def rerun(volumes):
     """ build rerun script and run if disk space free > 60 % and < 2 processes running """
-    volumes_added = check_running.main()
-    if len(volumes_added) < 2:
-        volumes_rerun = []
-        msg = []
+    volumes_dict = {'added': check_running.main(), 'rerun': [], 'msg': []}
+    if len(volumes_dict['added']) < 2:
         file = open(RERUN_SCRIPT, "w")
-        header = "#!/usr/bin/env bash\ncd /var/migration\nsource ~enstore/.bashrc\n"
-        file.write(header)
+        file.write("#!/usr/bin/env bash\ncd /var/migration\nsource ~enstore/.bashrc\n")
         logs = list_logs.get_logs('errors', volumes)
         for i, log in enumerate(logs):
             progress_bar.print_progress_bar(i, len(logs), prefix='Build Rerun:')
             command = ''
             ignore = False
             spool_full = False
-            file_name = MIGRATION_DIR + log
-            with open(file_name, 'rb') as fh:
-                first = next(fh).decode().split()
+            with open(MIGRATION_DIR + log, 'rb') as handle:
+                first = next(handle).decode().split()
                 volume = first[-1]
                 for argument in first:
                     if '/data/data' in argument:
                         spool = argument.split('/')
-                        total, used, free = shutil.disk_usage("/".join(spool[0:3]))
-                        if used/total > 0.60:
-                            msg.append("/".join(spool[0:3]) + ' is more than 60% full ' + volume)
+                        usage = shutil.disk_usage("/".join(spool[0:3]))
+                        if usage.used/usage.total > 0.60:
+                            volumes_dict['msg'].append("/".join(spool[0:3]) +
+                                                       ' is more than 60% full ' + volume)
                             spool_full = True
                             break
                     else:
                         if argument in IGNORE:
                             ignore = True
                             break
-                if volume not in volumes_added and not ignore and not spool_full:
+                if volume not in volumes_dict['added'] and not ignore and not spool_full:
                     command = "/opt/enstore/Python/bin/python " + " ".join(first[7:])
-                    volumes_added.append(volume)
+                    volumes_dict['added'].append(volume)
             if command != '':
                 file.write(command + '\n')
-                volumes_rerun.append(command.split()[-1])
+                volumes_dict['rerun'].append(command.split()[-1])
         file.close()
-        if len(msg) > 0:
-            print(msg)
+        if volumes_dict['msg']:
+            print(volumes_dict['msg'])
         os.chmod(RERUN_SCRIPT, 0o700)
         os.system('screen -d -m ' + RERUN_SCRIPT)
-        return len(volumes_rerun)
     else:
         print('too many migrate_chimera processes running')
-        return 0
+
+    return len(volumes_dict['rerun'])
 
 
 if __name__ == '__main__':
-    from migrate_helper_scripts import list_logs
     rerun(False)
-else:
-    from . import list_logs
--- migrate_helper_scripts/list_logs.py
+++ migrate_helper_scripts/list_logs.py
@@ -1,6 +1,6 @@
 """ list log files in migration directory """
 
-from os import stat, listdir
+from os import stat, scandir
 from pprint import pprint
 from datetime import datetime, timedelta
 from sys import argv
@@ -20,14 +20,12 @@ def check_file_mtime(file_name, age):
 
 
 def find_files(volume_serials, file_age):
-    """ find files """
-    file_names = listdir(LOG_DIRECTORY)
-    file_names.sort()
+    """ find files in log directory that look like MigrationLog and have a mtime """
     files_found = []
-    for file in file_names:
-        if LOG_PREFIX in file:
-            if check_file_mtime(file, file_age):
-                if len(volume_serials) > 0:
+    with scandir(LOG_DIRECTORY) as the_dir:
+        for file in the_dir:
+            if LOG_PREFIX in file and check_file_mtime(file, file_age):
+                if volume_serials:
                     for volume in volume_serials:
                         if volume in file:
                             files_found.append(file)
@@ -38,36 +36,46 @@ def find_files(volume_serials, file_age):
 
 
 def vol_prefix_in_file(file_name):
+    """ search volume in set of prefixes"""
     for vol_prefix in VOLUME_SERIAL_PREFIX:
         return bool(vol_prefix in file_name)
 
 
-def select_files(selection, file_list):
+def select_specific_files(selection, file_list):
+    """ selected files """
     selected = []
+    for file in file_list:
+        if selection == "errors":
+            if file.endswith(".0"):
+                selected.append(file)
+        elif selection == "no-errors":
+            if not file.endswith(".0"):
+                if not vol_prefix_in_file(file):
+                    selected.append(file)
+        elif selection == "archive":
+            if not file.endswith(".0"):
+                if vol_prefix_in_file(file):
+                    selected.append(file)
+        elif selection == "archive-with-errors":
+            if vol_prefix_in_file(file):
+                selected.append(file)
+
+    return selected
+
+
+def select_files(selection, file_list):
+    """ select files based on command """
     if selection == "all":
         selected = file_list
 
     else:
-        for file in file_list:
-            if selection == "errors":
-                if file.endswith(".0"):
-                    selected.append(file)
-            elif selection == "no-errors":
-                if not file.endswith(".0"):
-                    if not vol_prefix_in_file(file):
-                        selected.append(file)
-            elif selection == "archive":
-                if not file.endswith(".0"):
-                    if vol_prefix_in_file(file):
-                        selected.append(file)
-            elif selection == "archive-with-errors":
-                if vol_prefix_in_file(file):
-                    selected.append(file)
+        selected = select_specific_files(selection, file_list)
 
     return selected
 
 
 def get_logs(command="all", volumes=False):
+    """ main function determine file age if archiving """
     if not volumes:
         volumes = set()
     file_modified = FILE_MTIME_SHORT
@@ -79,5 +87,5 @@ def get_logs(command="all", volumes=False):
 
 
 if __name__ == '__main__':
-    files = get_logs(argv[1])
-    pprint(files, indent=1)
+    FILES = get_logs(argv[1])
+    pprint(FILES, indent=1)
