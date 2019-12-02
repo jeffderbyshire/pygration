6074b636eff9f798bace31bb1d455023ab629d22
jeffderb <jeffderb@fnal.gov>
2019-12-02 15:59:48 -0600

    new file process logs script

---
+++ migrate_helper_scripts/process_logs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MAILTO=jeffderb@fnal.gov
+source /opt/miniconda3/etc/profile.d/conda.sh
+source /root/.bashrc
+conda activate base
+cd /root/migration_scripts/tape-migration
+python pygration.py --process
