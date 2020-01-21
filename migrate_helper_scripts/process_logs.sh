89caa061ef153f9b98c6642e6e14630403f26562
jeffderb <jeffderb@fnal.gov>
2020-01-21 17:58:54 -0600

    add all argument to script

--- migrate_helper_scripts/process_logs.sh
+++ migrate_helper_scripts/process_logs.sh
@@ -4,4 +4,4 @@ source /opt/miniconda3/etc/profile.d/conda.sh
 source /root/.bashrc
 conda activate base
 cd /root/migration_scripts/tape-migration
-python pygration.py --process
+python pygration.py --process all
