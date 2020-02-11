d5caeed856d488b89633fb9a60c110ec50ff80b5
jeffderb <jeffderb@fnal.gov>
2020-02-11 17:17:34 -0600

    debug

--- migrate_helper_scripts/check_running.py
+++ migrate_helper_scripts/check_running.py
@@ -22,6 +22,7 @@ def main():
         ],
         capture_output=True,
     )
+    print(processes.stdout.decode())
     for process in processes.stdout.decode():
         print(process)
         exit()
