a49df88a5bafde73b3ca54b78938e572c0734bd1
jeffderb <jeffderb@fnal.gov>
2020-03-25 11:09:26 -0500

    debug with pathlib

--- migrate_helper_scripts/clean_spool.py
+++ migrate_helper_scripts/clean_spool.py
@@ -1,8 +1,7 @@
 """ clean spool area for volumes that have migrated """
 
 import logging
-import glob
-from os import scandir, path, unlink
+from pathlib import Path
 from configparser import ConfigParser
 from tqdm import tqdm
 import migrate_helper_scripts.parse_logs as parse_logs
@@ -18,17 +17,22 @@ def clean(quiet=False):
     """ check migration spool, find volumes and check migration status, then unlink """
     volume_serials = set()
     for number in range(1, 4):
-        dir_path = '/data/data' + str(number) + SPOOL_DIRECTORY
-        if path.isdir(dir_path):
-            with scandir(dir_path) as the_dir:
-                for file in tqdm(the_dir, desc='Checking Spool', disable=quiet):
-                    volume_serials.add(file.name.split(':')[0])
-                logging.info("spool contains %s volumes", len(volume_serials))
-                for volume in tqdm(volume_serials, desc='Checking volumes', disable=quiet):
-                    if database.volume_is_migrated(volume) \
-                            or parse_logs.check_migration_status(volume):
-                        logging.info("cleaning spool of volume:%s", volume)
-                        map(unlink, glob.glob(dir_path + volume + '*'))
+        try:
+            dir_path = Path('/data/data' + str(number) + SPOOL_DIRECTORY)
+        except OSError:
+            pass
+        for the_dir in dir_path.iterdir():
+            for file in tqdm(the_dir, desc='Checking Spool', disable=quiet):
+                print(file)
+                exit()
+                volume_serials.add(file.name.split(':')[0])
+            logging.info("spool contains %s volumes", len(volume_serials))
+            for volume in tqdm(volume_serials, desc='Checking volumes', disable=quiet):
+                if database.volume_is_migrated(volume) \
+                        or parse_logs.check_migration_status(volume):
+                    logging.info("cleaning spool of volume:%s", volume)
+                    for spool_file in dir_path.glob('/' + volume + '*'):
+                        spool_file.unlink()
 
 
 if __name__ == "__main__":
