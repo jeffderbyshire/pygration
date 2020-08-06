b2651934d9f74033734043d0bbddb3b85c932c05
jeffderb <jeffderb@fnal.gov>
2020-08-06 08:59:41 -0500

    debug

--- migrate_helper_scripts/reports.py
+++ migrate_helper_scripts/reports.py
@@ -1,8 +1,13 @@
 """ reports """
 
 import os
+from configparser import ConfigParser
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import migrate_helper_scripts.database_schema as database
+
+CONFIG = ConfigParser()
+CONFIG.read('config/config.conf')
+REPORT = CONFIG['Reports']['migration_report']
 
 
 def migration():
@@ -12,6 +17,5 @@ def migration():
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('migration_report.html')
-    result = database.get_migration_state_report()
-    print(result)
-    print(template.render(result=result))
+    with open(REPORT, "w") as file:
+        file.write(template.render(result=database.get_migration_state_report()))
